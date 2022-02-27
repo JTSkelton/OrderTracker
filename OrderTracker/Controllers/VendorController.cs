07fce7a2a12ebc7a5aef9b66278eafda12e502cc
Jack <skelton.jt9@gmail.com>
2022-02-27 14:26:43 -0500

    Added edit and delete functions. Added success alerts

--- OrderTracker/Controllers/VendorController.cs
+++ OrderTracker/Controllers/VendorController.cs
@@ -23,5 +23,83 @@ namespace OrderTracker.Controllers
         {
             return View();
         }
+
+        //POST
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult CreateVendor(Vendor obj)
+        {
+            if (ModelState.IsValid)
+            {
+                _db.Vendors.Add(obj);
+                _db.SaveChanges();
+                TempData["success"] = "Vendor Successfully Added!";
+                return RedirectToAction("Index");
+            }
+            return View(obj);
+            
+        }
+
+        //EDIT
+        public IActionResult Edit(int? id)
+        {
+            if (id == null || id == 0)
+            {
+                return NotFound();
+            }
+            var vendorFromDb = _db.Vendors.Find(id);
+            if (vendorFromDb == null)
+            {
+                return NotFound();
+            }
+            return View(vendorFromDb);
+        }
+
+        //EDIT
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Edit(Vendor obj)
+        {
+            if (ModelState.IsValid)
+            {
+                _db.Vendors.Update(obj);
+                _db.SaveChanges();
+                TempData["success"] = "Vendor Updated Successfully!";
+                return RedirectToAction("Index");
+            }
+            return View(obj);
+
+        }
+
+        //DELETE
+        public IActionResult Delete(int? id)
+        {
+            if (id == null || id == 0)
+            {
+                return NotFound();
+            }
+            var vendorFromDb = _db.Vendors.Find(id);
+            if (vendorFromDb == null)
+            {
+                return NotFound();
+            }
+            return View(vendorFromDb);
+        }
+
+        //DELETE
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult DeletePost(int? id)
+        {
+            var obj = _db.Vendors.Find(id);
+            if (obj == null)
+            { return NotFound();}
+
+            _db.Vendors.Remove(obj);
+            _db.SaveChanges();
+            TempData["success"] = "Vendor Deleted Successfully!";
+            return RedirectToAction("Index");
+
+        }
     }
 }
